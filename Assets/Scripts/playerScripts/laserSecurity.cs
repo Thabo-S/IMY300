e3e0e9408df2bd60f11a686fc5184457c56c9843
u23734176-del <u23734176@tuks.co.za>
2026-04-26 21:47:13 +0200

    add NPC sensor and Patrol Routes

--- Assets/Scripts/playerScripts/laserSecurity.cs
+++ Assets/Scripts/playerScripts/laserSecurity.cs
@@ -1,9 +1,25 @@
 using UnityEngine;
-
+using UnityEngine.Audio;
 public class LaserSecurity : MonoBehaviour
 {
+
+    private AudioSource audioSource;
     void Start()
     {
+
+
+
+        audioSource = GetComponent<AudioSource>();
+
+        if (audioSource == null)
+        {
+            Debug.LogError("There is no AudioSource on " + gameObject.name);
+        }
+        else
+        {
+            Debug.Log("AudioSource found on " + gameObject.name);
+        }
+
         Renderer renderer = GetComponent<Renderer>();
         if (renderer != null)
         {
@@ -22,5 +38,10 @@ public class LaserSecurity : MonoBehaviour
     void TriggerAlarm()
     {
         Debug.Log("ALARM! Laser tripped by the player!");
+
+        if (audioSource != null && !audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
     }
 }
