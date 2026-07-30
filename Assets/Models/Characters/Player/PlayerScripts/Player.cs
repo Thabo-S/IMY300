2d6fd3da8af1ec53547bc5f181eca27617175233
u24981712 <u24981712@tuks.co.za>
2026-07-30 14:24:40 +0200

    Fixed the door interaction and item highlighting

--- Assets/Models/Characters/Player/PlayerScripts/Player.cs
+++ Assets/Models/Characters/Player/PlayerScripts/Player.cs
@@ -86,4 +86,115 @@ public class Player : MonoBehaviour
         interaction.pitch = UnityEngine.Random.Range(minPitch, maxPitch);
         interaction.Play();
     }
+
+    // ====================== DO NOT TOUCH ======================
+    // ==========================================================
+    // ======== ONLY REFERENCE THE CODE ,DON'T MODIFY ===========
+
+    private GameObject currentHighlightedItem;
+    private GameObject currentHighlightedDoor;
+
+    public float pickUpRange = 3f;
+
+    void Update()
+    {
+        PerformContinuousDetection();
+    }
+
+
+    private void PerformContinuousDetection()
+    {
+        RaycastHit hit;
+        // We cast using the maximum of the two ranges so we don't miss anything
+
+        Debug.DrawRay(transform.position, transform.forward * pickUpRange, Color.red);
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, pickUpRange))
+        {
+            GameObject hitObject = hit.transform.gameObject;
+            float distance = hit.distance;
+
+            // --- Handle Items ---
+            if (hitObject.CompareTag("canPickUp") && distance <= pickUpRange)
+            {
+                if (hitObject != currentHighlightedItem)
+                {
+                    ClearItemHighlight();
+                    currentHighlightedItem = hitObject;
+                    ApplyItemHighlight(currentHighlightedItem);
+                }
+            }
+            else if (currentHighlightedItem != null) // If out of range or not hitting
+            {
+                ClearItemHighlight();
+            }
+
+            // --- Handle Doors ---
+            if (hitObject.CompareTag("Door_Left_Swing") || hitObject.CompareTag("Door_Right_Swing"))
+            {
+                if (hitObject != currentHighlightedDoor)
+                {
+                    ClearDoorHighlight();
+                    currentHighlightedDoor = hitObject;
+                    ApplyDoorHighlight(currentHighlightedDoor);
+                }
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    hitObject.GetComponent<doorMovement>().ToggleDoor();
+                }
+            }
+            else if (currentHighlightedDoor != null) // If out of range or not hitting
+            {
+                ClearDoorHighlight();
+            }
+        }
+        else
+        {
+            ClearItemHighlight();
+            ClearDoorHighlight();
+        }
+    }
+
+    private void ApplyItemHighlight(GameObject obj)
+    {
+        var outline = obj.GetComponent<Outline>();
+        if (outline != null)
+        {
+            //outline.OutlineColor = Color.white;
+            outline.enabled = true;
+        }
+    }
+
+    private void ClearItemHighlight()
+    {
+        if (currentHighlightedItem != null)
+        {
+            var outline = currentHighlightedItem.GetComponent<Outline>();
+            if (outline != null) outline.enabled = false;
+            currentHighlightedItem = null;
+        }
+    }
+
+    private void ApplyDoorHighlight(GameObject obj)
+    {
+        var outline = obj.GetComponent<Outline>();
+        if (outline != null)
+        {
+            //outline.OutlineColor = Color.yellow;
+            outline.enabled = true;
+        }
+    }
+
+    private void ClearDoorHighlight()
+    {
+        if (currentHighlightedDoor != null)
+        {
+            var outline = currentHighlightedDoor.GetComponent<Outline>();
+            if (outline != null) outline.enabled = false;
+            currentHighlightedDoor = null;
+        }
+    }
+
+
 }
